d511fe82017cccc416eefed7e1c5e5f6fbab219f
Akansha Agarwal <akansha_agarwal@tamu.edu>
2020-11-13 20:35:12 -0600

    duo server

---
+++ server/creds.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "ikey": "DI30JBOPVU4EKW6SJ9IQ",
+    "skey": "jW13V8xznuWK868twzja9O6pkvXmGfqkLQqGc5qI",
+    "akey": "677d3960a380d95145d1b2c378de6eb99a033eba",
+    "host": "api-bf099aad.duosecurity.com"
+}
